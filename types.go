8d14d5328d3ca1d9eab54164c800cea86856decd
Eli Saado <eli5saado@gmail.com>
2018-04-07 20:14:38 +0200

    Create AuthResponse type

--- types.go
+++ types.go
@@ -1,1 +1,12 @@
 package somtoday
+
+type AuthResponse struct {
+	AccessToken    string `json:"access_token"`
+	RefreshToken   string `json:"refresh_token"`
+	somtodayApiUrl string `json:"somtoday_api_url"`
+	Scope          string `json:"scope"`
+	SomtodayTenant string `json:"somtoday_tenant"`
+	IdToken        string `json:"id_token"`
+	TokenType      string `json:"token_type"`
+	ExpiresIn      int    `json:"expires_in"`
+}
