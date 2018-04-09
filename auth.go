baf8098a4e4adf5e6908a71af59b2cfc8eb55700
Eli Saado <eli5saado@gmail.com>
2018-04-09 11:10:40 +0200

    You can now refresh and test refresh

--- auth.go
+++ auth.go
@@ -43,3 +43,37 @@ func Login(school Organisation, username, password string) (response AuthRespons
 
 	return
 }
+
+// RefreshTokens refreshes the tokens
+func RefreshTokens(refreshToken string) (response AuthResponse, err error) {
+	client := &http.Client{}
+	params := url.Values{}
+	params.Set("grant_type", "refresh_token")
+	params.Set("client_id", AppID)
+	params.Set("client_secret", AppSecret)
+	params.Set("refresh_token", refreshToken)
+
+	req, err := http.NewRequest("POST", "https://production.somtoday.nl/oauth2/token", strings.NewReader(params.Encode()))
+	if err != nil {
+		return
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Accept", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return
+	}
+
+	return
+}
--- auth_test.go
+++ auth_test.go
@@ -25,3 +25,39 @@ func TestLogin(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestRefreshTokens(t *testing.T) {
+	// Get auth tokens
+	organisation, err := GetOrganisation("Bonhoeffer College")
+	t.Log(organisation)
+	t.Log(err)
+	if err != nil {
+		t.Fail()
+	}
+	auth, err := Login(organisation, os.Getenv("SOM_USERNAME"), os.Getenv("SOM_PASSWORD")) // safe
+	t.Log(auth)
+	t.Log(err)
+	if err != nil {
+		t.Fail()
+	}
+
+	// Check if it fetched correctly
+	if auth.SomtodayTenant != "bonhoeffer" {
+		t.Log("Did not fetch correctly")
+		t.Fail()
+	}
+
+	// Refresh them
+	newAuth, err := RefreshTokens(auth.RefreshToken)
+	t.Log(newAuth)
+	t.Log(err)
+	if err != nil {
+		t.Fail()
+	}
+
+	// Check if the tokens from old and new are the same
+	if auth.AccessToken == newAuth.AccessToken {
+		t.Log("Tokens are the same, it did not refresh")
+		t.Fail()
+	}
+}
